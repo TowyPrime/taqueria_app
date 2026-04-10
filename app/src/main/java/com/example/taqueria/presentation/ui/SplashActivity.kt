b6f67d1407ab88b545a4c8b291cf3277c1857770
Angel <angeldti12@gmail.com>
2026-04-09 18:40:28 -0600

    second commit of taqueria app

---
+++ app/src/main/java/com/example/taqueria/presentation/ui/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.taqueria.presentation.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import com.example.taqueria.R
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}
