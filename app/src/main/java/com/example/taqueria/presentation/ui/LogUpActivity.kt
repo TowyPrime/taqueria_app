9a34c15de453f0f4ecad76bd8f521fec49da122a
Angel <angeldti12@gmail.com>
2026-04-11 21:49:41 -0600

    third commit of taqueria app

--- app/src/main/java/com/example/taqueria/presentation/ui/LogUpActivity.kt
+++ app/src/main/java/com/example/taqueria/presentation/ui/LogUpActivity.kt
@@ -1,36 +1,21 @@
 package com.example.taqueria.presentation.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taqueria.R
 
-class SplashActivity : AppCompatActivity() {
+class LogUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_log_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setupSplash()
-    }
-
-    private fun setupSplash() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            showLogin()
-        },6000)
-    }
-
-    fun showLogin() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
     }
 }
